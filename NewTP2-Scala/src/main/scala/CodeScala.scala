01c7ee3508b54f605850118db0ea8c740ade70ec
Ambre <ambre_donnadieu@live.fr>
2019-12-14 22:42:29 +0100

    Scala code mais pas encore au point

--- NewTP2-Scala/src/main/scala/CodeScala.scala
+++ NewTP2-Scala/src/main/scala/CodeScala.scala
@@ -2,21 +2,41 @@ import org.apache.spark.api.java.JavaRDD
 import org.apache.spark.sql.{Dataset, Row, SparkSession}
 import org.apache.spark.sql.functions.col
 
+import scala.collection.mutable
+import scala.collection.mutable.ArrayBuffer
+
 object CodeScala extends App {
 
   def EXO1_RDD(): Unit = {
     val spark = SparkSession.builder.appName("Java Spark SQL basic example").config("spark.master", "local").getOrCreate
 
-    val monsters = spark.read.json("AllMonsterBestiary.json")
-    monsters.printSchema()
-    monsters.createOrReplaceTempView("MonsterSpells")
+    val monsters = spark.read.json("C:\\Users\\Ambre\\Desktop\\TP2BDDScala\\NewTP2-Scala\\AllMonsterBestiary.json")
+    //monsters.printSchema()
+    //monsters.createOrReplaceTempView("MonsterSpells")
     //monsters.select(col("Name"), col("Sorts")).show();
 
 
     val test = monsters.toJavaRDD
-    monsters.select(col("Name"), col("Sorts")).show()
+    //monsters.select(col("Name"), col("Sorts")).show()
     //val test2 = test.map(  )
+
+
+    val r1=monsters.flatMap(creature  =>{
+
+     // val resultats = new ArrayBuffer[Tuple2[String,String]]
+      val resultats = new ArrayBuffer[(String, String)]
+      for(i<- 1 to 10) {
+        resultats += Tuple2(creature(i).toString, creature(i).toString)
+      }
+
+    })()
+    val schem2 = println(test)
+
+
+
   }
 
-
+  override def main(args: Array[String]): Unit = {
+    EXO1_RDD()
+  }
 }
