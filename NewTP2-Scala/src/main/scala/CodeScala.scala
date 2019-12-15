ad84e9daa9606207ea4f87e3dbc69ac1fb41efd8
Ambre <ambre_donnadieu@live.fr>
2019-12-15 18:37:29 +0100

    Dernière modif mais avec encore un bug

--- NewTP2-Scala/src/main/scala/CodeScala.scala
+++ NewTP2-Scala/src/main/scala/CodeScala.scala
@@ -7,36 +7,33 @@ import scala.collection.mutable.ArrayBuffer
 
 object CodeScala extends App {
 
-  def EXO1_RDD(): Unit = {
+  def EXO1_RDD() {
     val spark = SparkSession.builder.appName("Java Spark SQL basic example").config("spark.master", "local").getOrCreate
+    val monsters = spark.read.json("C:/Users/Ambre/Desktop/TP2BDDScala/NewTP2-Scala/AllMonsterBestiary.json").toDF()
 
-    val monsters = spark.read.json("C:\\Users\\Ambre\\Desktop\\TP2BDDScala\\NewTP2-Scala\\AllMonsterBestiary.json")
     //monsters.printSchema()
-    //monsters.createOrReplaceTempView("MonsterSpells")
     //monsters.select(col("Name"), col("Sorts")).show();
+    val test = monsters.printSchema()
 
-
-    val test = monsters.toJavaRDD
-    //monsters.select(col("Name"), col("Sorts")).show()
-    //val test2 = test.map(  )
-
-
-    val r1=monsters.flatMap(creature  =>{
-
-     // val resultats = new ArrayBuffer[Tuple2[String,String]]
-      val resultats = new ArrayBuffer[(String, String)]
+    val r1 = monsters.map( creature => {
+      val resultats = new ArrayBuffer[Tuple2[String,String]]
+      val name = creature.getAs[String]("Name")
+      val sort = creature.getAs[Array[String]]("Sorts")
       for(i<- 1 to 10) {
-        resultats += Tuple2(creature(i).toString, creature(i).toString)
+        val resultat3 = (name,sort(i))
+        resultats+=resultat3
       }
+    })
 
-    })()
+
+    //dataset = [Name:String, Sorts : Array<String>]
+    val df = monsters.toDF("Name", "Sorts")
+   // df.flatMap(v=> v._2.map(g => (v._1, g)))
     val schem2 = println(test)
-
-
 
   }
 
-  override def main(args: Array[String]): Unit = {
+  override def main(args: Array[String]) {
     EXO1_RDD()
   }
 }
