9e5214e4b0f574c3007a3fb7bdc32c1d49234dde
Ambre <ambre_donnadieu@live.fr>
2019-12-15 22:46:23 +0100

    Flatmap

--- NewTP2-Scala/src/main/scala/CodeScala.scala
+++ NewTP2-Scala/src/main/scala/CodeScala.scala
@@ -12,13 +12,12 @@ object CodeScala extends App {
     val monsters = spark.read.json("C:/Users/Ambre/Desktop/TP2BDDScala/NewTP2-Scala/AllMonsterBestiary.json").toDF()
 
     //val monsters = spark.read.json("C:\\Users\\Ambre\\Desktop\\TP2BDDScala\\NewTP2-Scala\\AllMonsterBestiary.json")
-    val monsters = spark.read.json("C:\\Users\\aajin\\IdeaProjects\\TP2BDD\\TP2BDDScala\\NewTP2-Scala\\AllMonsterBestiary.json")
-
+    //val monsters = spark.read.json("C:\\Users\\aajin\\IdeaProjects\\TP2BDD\\TP2BDDScala\\NewTP2-Scala\\AllMonsterBestiary.json")
     //monsters.printSchema()
     //monsters.select(col("Name"), col("Sorts")).show();
     val test = monsters.printSchema()
 
-    val r1 = monsters.map( creature => {
+   /* val r1 = monsters.rdd.map( creature => {
       val resultats = new ArrayBuffer[Tuple2[String,String]]
       val name = creature.getAs[String]("Name")
       val sort = creature.getAs[Array[String]]("Sorts")
@@ -26,9 +25,15 @@ object CodeScala extends App {
         val resultat3 = (name,sort(i))
         resultats+=resultat3
       }
-    })
+    })*/
 
+    //val splitMe = List( ("key1", List(1,2,3)), ("key2", List(4,5)) )
+    //val result = splitMe.flatMap(v=> v._2.map(g => (v._1, g)))
 
+    val preresult = monsters.collect()
+    val result = monsters.collect().flatMap(v => v.getAs[mutable.WrappedArray[String]]("Sorts").map(g => (v.getAs[String]("Name"), g)))
+    val j =2+2
+    print(j.toString())
     //dataset = [Name:String, Sorts : Array<String>]
     val df = monsters.toDF("Name", "Sorts")
    // df.flatMap(v=> v._2.map(g => (v._1, g)))
